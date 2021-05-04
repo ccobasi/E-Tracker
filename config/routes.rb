3d4512836f69a6630fac449a375c3317056dc6d2
Chukwuma Obasi <ccobasi8@gmail.com>
2021-05-04 16:00:53 +0100

    Create project model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :groups
+  resources :projects, only: %i[show destroy new create index]
+  resources :groups, only: %i[show destroy new create index update edit]
   resources :users, only: %i[show update destroy index edit new create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'users#index'
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_04_144904) do
+ActiveRecord::Schema.define(version: 2021_05_04_145541) do
 
   create_table "groups", force: :cascade do |t|
     t.string "name"
     t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.integer "author_id"
+    t.integer "duration"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
