34a16aed256f9f14f17653d1277bed79016c8b5b
Chukwuma Obasi <ccobasi8@gmail.com>
2021-05-04 13:56:04 +0100

    Add validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    has_many :expenses, dependent: :destroy
+    has_many :groups, dependent: :destroy
+
+    has_one_attached :avatar, dependent: :destroy
+
+    validates :email, presence: true
+    validates :name, presence: true, uniqueness: true
 end
