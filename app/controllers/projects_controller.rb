65cab2cf972b6688e0a29af0eaed758be7ee3cfa
Chukwuma Obasi <ccobasi8@gmail.com>
2021-05-15 12:02:47 +0100

    Update project helper

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -81,7 +81,7 @@ class ProjectsController < ApplicationController
 
   # Use callbacks to share common setup or constraints between actions.
   def set_project
-    @project = Project.find(params[:project_id])
+    @project = Project.find(params[:id])
   end
 
   # Only allow a list of trusted parameters through.
